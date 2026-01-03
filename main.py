017f6ad4868b595fe49acc55b7086309e4a721aa
iandinoo <nuriadeviana@gmail.com>
2026-01-04 04:19:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
-from router import router
+from router import register_routes
 
-app = Client("filesharingbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client(
+    "filesharingbot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
 
-# Register all handlers
-router.register(app)
+register_routes(app)
 
 if __name__ == "__main__":
-    print("Bot starting ...")
+    print("🤖 FileSharingBot started")
     app.run()
     
