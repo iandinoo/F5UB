fca337714c364a6d7fbf50b91cbde4e3a1827fe1
iandinoo <nuriadeviana@gmail.com>
2026-01-03 22:31:33 +0700

    Initialize bot with Pyrogram and register handlers

---
+++ main.py
@@ -0,0 +1,13 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+from router import router
+
+app = Client("filesharingbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+# Register all handlers
+router.register(app)
+
+if __name__ == "__main__":
+    print("Bot starting ...")
+    app.run()
+  
