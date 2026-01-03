ca95f3f8635f94111779a5b201f2dd609f8f53c7
iandinoo <nuriadeviana@gmail.com>
2026-01-03 22:51:44 +0700

    Add utility functions for link generation and formatting

---
+++ modules/utils.py
@@ -0,0 +1,9 @@
+def generate_link(file_id):
+    return f"https://t.me/YourBot?start={file_id}"
+
+def format_users(users):
+    return "\n".join([str(u["_id"]) for u in users])
+
+def format_admins(admins):
+    return "\n".join([str(a["_id"]) for a in admins])
+  
