bc0145cd61ca23afc63e1b3b200e54b4e0c07b89
iandinoo <nuriadeviana@gmail.com>
2026-01-03 22:49:43 +0700

    Create __init__.py with package description
    Add module documentation for file sharing bot.

---
+++ modules/__init__.py
@@ -0,0 +1,8 @@
+"""
+Package modules untuk File Sharing Bot.
+
+Berisi:
+- handlers.py  -> semua handler bot
+- database.py  -> semua fungsi CRUD MongoDB
+- utils.py     -> helper dan link generator
+"""
