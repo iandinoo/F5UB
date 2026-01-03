712873b287dac12d2f3770b8bc46c56099a20873
iandinoo <nuriadeviana@gmail.com>
2026-01-04 00:27:46 +0700

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,8 +1,1 @@
-"""
-Package modules untuk File Sharing Bot.
-
-Berisi:
-- handlers.py  -> semua handler bot
-- database.py  -> semua fungsi CRUD MongoDB
-- utils.py     -> helper dan link generator
-"""
+"""Package modules untuk File Sharing Bot."""
