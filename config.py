0bc24b4aade6bba91ab57b4725986033199395ff
iandinoo <nuriadeviana@gmail.com>
2026-01-04 04:17:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,18 @@
 import os
 from dotenv import load_dotenv
 
-# Load file .env jika ada
-load_dotenv()
+load_dotenv()  # load .env otomatis
 
-API_ID = int(os.getenv("API_ID", 0))
+def _int(name, default=0):
+    try:
+        return int(os.getenv(name, default))
+    except ValueError:
+        return default
+
+API_ID = _int("API_ID")
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-OWNER_ID = int(os.getenv("OWNER_ID", 0))
-DATABASE_CHANNEL = int(os.getenv("DATABASE_CHANNEL", 0))
+
+OWNER_ID = _int("OWNER_ID")
+DATABASE_CHANNEL = _int("DATABASE_CHANNEL")
 MONGO_URI = os.getenv("MONGO_URI", "")
