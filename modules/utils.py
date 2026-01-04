6e94aaa2168f668e295bad83980ec0e2ab3057de
iandinoo <nuriadeviana@gmail.com>
2026-01-04 10:28:18 +0700

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -1,9 +1,6 @@
-def generate_link(file_id):
-    return f"https://t.me/YourBot?start={file_id}"
+import random
+import string
 
-def format_users(users):
-    return "\n".join([str(u["_id"]) for u in users])
-
-def format_admins(admins):
-    return "\n".join([str(a["_id"]) for a in admins])
+def generate_code(length=8):
+    return "".join(random.choices(string.ascii_letters + string.digits, k=length))
     
