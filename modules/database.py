9c756492d4191e1f15a15625407696e87d9e18ac
iandinoo <nuriadeviana@gmail.com>
2026-01-04 10:26:47 +0700

    Update database.py

--- modules/database.py
+++ modules/database.py
@@ -1,27 +1,74 @@
 from pymongo import MongoClient
-from config import MONGO_URI
+from config import MONGODB_URL
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(MONGODB_URL)
 db = client["filesharingbot"]
 
-users = db.users
-admins = db.admins
-files = db.files
 settings = db.settings
-forcesub = db.forcesub
+admins = db.admins
+forcesubs = db.forcesubs
+users = db.users
+files = db.files
 
-def add_user(uid):
-    users.update_one({"_id": uid}, {"$set": {}}, upsert=True)
 
-def get_users():
-    return list(users.find())
+def get_setting(key):
+    data = settings.find_one({"_id": key})
+    return data["value"] if data else None
 
-def add_admin(uid):
-    admins.update_one({"_id": uid}, {"$set": {}}, upsert=True)
+
+def set_setting(key, value):
+    settings.update_one(
+        {"_id": key},
+        {"$set": {"value": value}},
+        upsert=True
+    )
+
+
+# ---------- ADMINS ----------
+def add_admin(user_id):
+    admins.update_one({"_id": user_id}, {"$set": {}}, upsert=True)
+
+
+def del_admin(user_id):
+    admins.delete_one({"_id": user_id})
+
 
 def get_admins():
-    return list(admins.find())
+    return [x["_id"] for x in admins.find()]
 
-def remove_admin(uid):
-    admins.delete_one({"_id": uid})
-    
+
+# ---------- FORCE SUB ----------
+def add_force_sub(chat_id):
+    forcesubs.update_one({"_id": chat_id}, {"$set": {}}, upsert=True)
+
+
+def del_force_sub(chat_id):
+    forcesubs.delete_one({"_id": chat_id})
+
+
+def get_force_subs():
+    return [x["_id"] for x in forcesubs.find()]
+
+
+# ---------- USERS ----------
+def save_user(user_id):
+    users.update_one({"_id": user_id}, {"$set": {}}, upsert=True)
+
+
+def del_user(user_id):
+    users.delete_one({"_id": user_id})
+
+
+def get_all_users():
+    return [x["_id"] for x in users.find()]
+
+
+# ---------- FILES ----------
+def save_file(code, message_id):
+    files.insert_one({"_id": code, "msg_id": message_id})
+
+
+def get_file(code):
+    data = files.find_one({"_id": code})
+    return data["msg_id"] if data else None
+                      
