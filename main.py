df73ef118cc24f7789d4e0a6c099fd9063618105
iandinoo <nuriadeviana@gmail.com>
2026-01-04 12:29:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from config import API_ID, API_HASH, BOT_TOKEN
 from router import register_routes
 
 app = Client(
-    "filesharingbot",
+    "F5UB",
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=BOT_TOKEN
+    bot_token=BOT_TOKEN,
+    in_memory=True
 )
 
 register_routes(app)
