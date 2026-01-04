23211aab3ca381a31b81542f311926d6c1325955
iandinoo <nuriadeviana@gmail.com>
2026-01-04 14:00:48 +0700

    Update force_sub.py

--- modules/force_sub.py
+++ modules/force_sub.py
@@ -1,20 +1,12 @@
-from pyrogram.enums import ChatMemberStatus
-from config import FORCE_SUB_CHANNELS
+from pyrogram.errors import UserNotParticipant
+from modules.database import forcesub
 
-async def check_force_sub(client, user_id):
-    not_joined = []
-
-    for chat_id in FORCE_SUB_CHANNELS:
+async def check_force_sub(client, user_id: int):
+    chans = list(forcesub.find())
+    for c in chans:
         try:
-            member = await client.get_chat_member(chat_id, user_id)
-            if member.status not in (
-                ChatMemberStatus.MEMBER,
-                ChatMemberStatus.ADMINISTRATOR,
-                ChatMemberStatus.OWNER
-            ):
-                not_joined.append(chat_id)
-        except:
-            not_joined.append(chat_id)
-
-    return not_joined
+            await client.get_chat_member(c["_id"], user_id)
+        except UserNotParticipant:
+            return False
+    return True
     
