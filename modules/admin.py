de6104a495d51dfee7dff341817ced79834cb3e7
iandinoo <nuriadeviana@gmail.com>
2026-01-04 14:02:30 +0700

    Update admin.py

--- modules/admin.py
+++ modules/admin.py
@@ -1,16 +1,8 @@
-from pyrogram import filters
-from config import OWNER_ID, DATABASE_CHAT_ID
-from modules.database import save_file
-from modules.utils import generate_token
+from config import OWNER_ID
+from modules.database import admins
 
-def register_admin(app):
-
-    @app.on_message(filters.private & filters.user(OWNER_ID) & filters.document)
-    async def upload_file(client, message):
-        sent = await message.forward(DATABASE_CHAT_ID)
-        token = generate_token()
-        save_file(token, sent.document.file_id)
-
-        link = f"https://t.me/{(await client.get_me()).username}?start={token}"
-        await message.reply(f"✅ File tersimpan\n\n🔗 Link:\n{link}")
-      
+def is_admin(user_id: int):
+    if user_id == OWNER_ID:
+        return True
+    return admins.find_one({"_id": user_id}) is not None
+    
