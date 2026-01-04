4364f5c442ac49e7eaf0c0c266237cdbe69ea3ef
iandinoo <nuriadeviana@gmail.com>
2026-01-04 10:52:30 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+def must_env(key):
+    value = os.getenv(key)
+    if not value:
+        raise RuntimeError(f"ENV {key} belum diisi")
+    return value
 
-OWNER_ID = int(os.getenv("OWNER_ID"))
-DATABASE_CHAT_ID = int(os.getenv("DATABASE_CHAT_ID"))
+API_ID = int(must_env("API_ID"))
+API_HASH = must_env("API_HASH")
+BOT_TOKEN = must_env("BOT_TOKEN")
 
-MONGODB_URL = os.getenv("MONGODB_URL")
+OWNER_ID = int(must_env("OWNER_ID"))
+DATABASE_CHAT_ID = int(must_env("DATABASE_CHAT_ID"))
+
+MONGODB_URL = must_env("MONGODB_URL")
